f416fc783a71319c9bc12f5806c83f54069a7e01
Farglabs <martel@vilmal.com>
2022-12-22 18:34:57 -0800

    Added a couple other listeners on independent threads

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 use std::net::TcpListener;
+use std::thread;
 
 fn main() {
-    let listener = TcpListener::bind("0.0.0.0:80").unwrap();
-    for stream in listener.incoming() {
+    thread::spawn(|| {
+        let listener445 = TcpListener::bind("0.0.0.0:445").unwrap();
+        for stream in listener445.incoming() {
+            let stream = stream.unwrap();
+            println!("CO!");
+        }
+    });
+    thread::spawn(|| {
+        let listener80 = TcpListener::bind("0.0.0.0:80").unwrap();
+        for stream in listener80.incoming() {
+            let stream = stream.unwrap();
+            //let req = stream.req.remote();
+            //let remote = listener.req.remote();
+            println!("H2S!");
+        }
+    });
+    println!("Started everything we could.");
+    let listener443 = TcpListener::bind("0.0.0.0:443").unwrap();
+    for stream in listener443.incoming() {
         let stream = stream.unwrap();
         //let req = stream.req.remote();
         //let remote = listener.req.remote();
-        println!("H2S!");
+        println!("CH4!");
     }
 }
